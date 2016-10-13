d5d1df5f63189a9ee6cd1ece4a5f01035d38635d
adamharvey <adam.harvey@octanner.com>
2016-10-13 15:00:29 -0600

    initial checkin.

---
+++ src/java/com/octanner/testTomcat/TestTomcatServlet.java
@@ -0,0 +1,19 @@
+package com.octanner.testTomcat;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class TestTomcatServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest request,
+					  HttpServletResponse response)
+			throws ServletException,IOException{
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html>");
+		pw.println("<head><title>Hello World</title></title>");
+		pw.println("<body>");
+		pw.println("<h1>Hello World</h1>");
+		pw.println("</body></html>");
+	}
+}
